0fc56c0c863f9a8224d4a8efc5b8cc15cc80907d
Projekt_druhy_rocnik <jan.kalisz007@gmail.com>
2024-02-28 12:14:42 +0100

    Basic working

--- src/JFileChooserForm.java
+++ src/JFileChooserForm.java
@@ -6,36 +6,40 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
 
-public class JFileChooserForm extends JFrame{
-    private JButton button1;
+public class JFileChooserForm extends JFrame {
+    private JButton openBtn;
     private JPanel panel1;
+    private JTextArea txt;
 
-    public JFileChooserForm(){
+    public JFileChooserForm() {
         setContentPane(panel1);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(300,300);
-        button1.addActionListener(e -> {
-            JFileChooser fc = new JFileChooser(".");
-            fc.setFileFilter(new FileNameExtensionFilter("Project files", "txt"));
-            int vysledek = fc.showOpenDialog(this);
-            if (vysledek == JFileChooser.APPROVE_OPTION) {
-                File zvolenySoubor = fc.getSelectedFile();
-                StringBuilder content = new StringBuilder();
-                try(Scanner sc = new Scanner(new BufferedReader(new FileReader(zvolenySoubor)))){
-                    String line;
-                    while (sc.hasNextLine()) {
-                        content.append(sc.nextLine()).append("\n");
-                    }
-                    JOptionPane.showMessageDialog(this, content.toString());
-                } catch (FileNotFoundException ex) {
-                    throw new RuntimeException(ex);
-                }
-            }
-        });
+        setSize(700, 700);
+        txt.setEditable(false);
+        /**openBtn.addActionListener(e -> {
+         ChooseFile();
+         });*/
     }
 
-    public static void main(String[] args) {
-        JFileChooserForm j = new JFileChooserForm();
-        j.setVisible(true);
+    public void ChooseFile() {
+        JFileChooser fc = new JFileChooser(".");
+        fc.setFileFilter(new FileNameExtensionFilter("Project files", "txt"));
+        int vysledek = fc.showOpenDialog(this);
+        if (vysledek == JFileChooser.APPROVE_OPTION) {
+            File zvolenySoubor = fc.getSelectedFile();
+            StringBuilder content = new StringBuilder();
+            try (Scanner sc = new Scanner(new BufferedReader(new FileReader(zvolenySoubor)))) {
+                while (sc.hasNextLine()) {
+                    content.append(sc.nextLine()).append("\n");
+                }
+                txt.setText(String.valueOf(content));
+            } catch (FileNotFoundException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+    }
+    public static void main (String[]args){
+            JFileChooserForm j = new JFileChooserForm();
+            j.setVisible(true);
     }
 }
